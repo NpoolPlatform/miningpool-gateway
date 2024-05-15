7deeba12e5f17c5a47230d5520b97909d1e0f6d6
Geapefurit <670884108@qq.com>
2024-05-15 04:51:10 +0000

    fixbug

--- api/rootuser/query.go
+++ api/rootuser/query.go
@@ -12,35 +12,6 @@ import (
 	npool "github.com/NpoolPlatform/message/npool/miningpool/gw/v1/rootuser"
 )
 
-func (s *Server) AdminGetRootUser(ctx context.Context, in *npool.AdminGetRootUserRequest) (*npool.AdminGetRootUserResponse, error) {
-	handler, err := rootuser1.NewHandler(
-		ctx,
-		rootuser1.WithEntID(&in.EntID, true),
-	)
-	if err != nil {
-		logger.Sugar().Errorw(
-			"GetRootUser",
-			"In", in,
-			"Error", err,
-		)
-		return &npool.AdminGetRootUserResponse{}, status.Error(codes.Aborted, err.Error())
-	}
-
-	info, err := handler.GetRootUser(ctx)
-	if err != nil {
-		logger.Sugar().Errorw(
-			"GetRootUser",
-			"In", in,
-			"Error", err,
-		)
-		return &npool.AdminGetRootUserResponse{}, status.Error(codes.Aborted, err.Error())
-	}
-
-	return &npool.AdminGetRootUserResponse{
-		Info: info,
-	}, nil
-}
-
 func (s *Server) AdminGetRootUsers(ctx context.Context, in *npool.AdminGetRootUsersRequest) (*npool.AdminGetRootUsersResponse, error) {
 	handler, err := rootuser1.NewHandler(
 		ctx,
